da04646bd528e8cff0d429c23fe40b9f3177ae5b
Michael Heilmann <michael.heilmann@shizu.org>
2024-04-20 23:34:08 +0200

    add color definitions
    add definitions for
    - black and white
    - the three primary colors
    - the three secondary colors

--- includes/color.h
+++ includes/color.h
@@ -14,9 +14,9 @@
 typedef struct idlib_color_3_u8 {
 	union {
 		struct {
-			uint8_t r, g, b;
+			idlib_u8 r, g, b;
 		};
-		uint8_t components[3];
+		idlib_u8 components[3];
 	};
 } idlib_color_3_u8;
 
@@ -24,32 +24,84 @@ typedef struct idlib_color_3_u8 {
  * @since 1.2
  * @brief Assign an idlib_color_3_u8 object the specified component values.
  * @param target Pointer to the idlib_matrix_4x4_f32 object.
- * @param r The value to be assigned to the "red" component.
- * @param g The value to be assigned to the "green" component.
- * @param b The value to be assigned to the "blue" component.
+ * @param operand1 The value to be assigned to the "red" component.
+ * @param operand2 The value to be assigned to the "green" component.
+ * @param operand3 The value to be assigned to the "blue" component.
  */
 static inline void
 idlib_color_3_u8_set
 	(
 		idlib_color_3_u8 *target,
-		idlib_u8 r,
-		idlib_u8 g,
-		idlib_u8 b
+		idlib_u8 operand1,
+		idlib_u8 operand2,
+		idlib_u8 operand3
 	);
 
 static inline void
 idlib_color_3_u8_set
 	(
 		idlib_color_3_u8 *target,
-		idlib_u8 r,
-		idlib_u8 g,
-		idlib_u8 b
+		idlib_u8 operand1,
+		idlib_u8 operand2,
+		idlib_u8 operand3
 	)
 {
 	IDLIB_DEBUG_ASSERT(NULL != target);
-	target->r = r;
-	target->g = g;
-	target->b = b;
+	target->r = operand1;
+	target->g = operand2;
+	target->b = operand3;
+}
+
+/**
+ * @since 1.2
+ * @brief
+ * Representation of a color consisting of three components, red, green, blue, and alpha.
+ * The component values are layed out consecutively in memory and are of type idlib_f32 where
+ * 0 denotes the minimum intensity and 1 denotes the maximum intensity.
+ */
+typedef struct idlib_color_4_f32 {
+	union {
+		struct {
+			idlib_f32 r, g, b, a;
+		};
+		idlib_f32 components[4];
+	};
+} idlib_color_4_f32;
+
+/**
+ * @since 1.2
+ * @brief Assign an idlib_color_4_f32 object the specified component values.
+ * @param target Pointer to the idlib_matrix_4x4_f32 object.
+ * @param r The value to be assigned to the "red" component.
+ * @param g The value to be assigned to the "green" component.
+ * @param b The value to be assigned to the "blue" component.
+ * @param a The value to be assigned to the "alpha" component.
+ */
+static inline void
+idlib_color_4_f32_set
+	(
+		idlib_color_4_f32* target,
+		idlib_f32 operand1,
+		idlib_f32 operand2,
+		idlib_f32 operand3,
+		idlib_f32 operand4
+	);
+
+static inline void
+idlib_color_4_f32_set
+	(
+		idlib_color_4_f32* target,
+		idlib_f32 operand1,
+		idlib_f32 operand2,
+		idlib_f32 operand3,
+		idlib_f32 operand4
+	)
+{
+	IDLIB_DEBUG_ASSERT(NULL != target);
+	target->r = operand1;
+	target->g = operand2;
+	target->b = operand3;
+	target->a = operand4;
 }
 
 #endif // IDLIB_COLOR_H_INCLUDED
---
+++ includes/colors.i
@@ -0,0 +1,13 @@
+DEFINE(black, 0, 0, 0)       // https://drafts.csswg.org/css-color/#named-colors
+DEFINE(white, 255, 255, 255) // https://drafts.csswg.org/css-color/#named-colors
+
+// primary colors
+DEFINE(red, 255, 0, 0)       // https://drafts.csswg.org/css-color/#named-colors
+DEFINE(green, 0, 255, 0)     // https://drafts.csswg.org/css-color/#named-colors
+DEFINE(blue, 0, 0, 255)      // https://drafts.csswg.org/css-color/#named-colors
+
+// secondary colors
+DEFINE(yellow, 255, 255, 0)  // https://drafts.csswg.org/css-color/#named-colors
+DEFINE(magenta, 255, 0, 255) // https://drafts.csswg.org/css-color/#named-colors
+DEFINE(aqua, 0, 255, 255)    // https://drafts.csswg.org/css-color/#named-colors
+DEFINE(cyan, 0, 255, 255)    //
