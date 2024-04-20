5f56de7091541619fe74cb8b21faefacf6ca8712
Michael Heilmann <michael.heilmann@shizu.org>
2024-04-20 23:34:02 +0200

    add 'version.(c|h)'

--- includes/idlib.h
+++ includes/idlib.h
@@ -2,15 +2,8 @@
 #if !defined(IDLIB_H_INCLUDED)
 #define IDLIB_H_INCLUDED
 
+#include "version.h"
 #include <assert.h>
-
-/// @since 1.0
-/// The major version number of this library.
-#define IDLIB_VERSION_MAJOR 1
-
-/// @since 1.0
-/// The minor version number of this library.
-#define IDLIB_VERSION_MINOR 0
 
 /// @since 1.0
 /// Alias for float.
