6c42f203a95324dc7f97023765fd5af2cc2c8838
Michael Heilmann <michael.heilmann@shizu.org>
2024-04-20 23:34:06 +0200

    add 'set_ortho' to 'matrix_4x4.h'

--- includes/matrix_4x4.h
+++ includes/matrix_4x4.h
@@ -110,6 +110,49 @@ idlib_matrix_4x4_f32_set_rotation_z
 		idlib_f32 operand
 	);
 
+/// @since 1.0
+/// @brief Assign this matrix the values of an orthographic projection matrix.
+/// @param target A pointer to this matrix.
+/// @param left The distance to the left clip plane.
+/// @param right The distance to the right clip plane.
+/// @param bottom The distance to the bottom clip plane.
+/// @param top The distance to the top clip plane.
+/// @param near The distance to the near clip plane.
+/// @param far The distance to the far clip plane.
+/// @remarks
+/// The following matrix is created
+/// @code
+/// | 2/a | 0   | 0     | u |
+/// | 0   | 2/b | 0     | v |
+/// | 0   | 0   | - 2/c | w |
+/// | 0   | 0   | 0     | 1 |
+/// @endcode
+/// where
+/// @code
+/// a = right - left
+/// b = top - bottom
+/// c = far - near
+/// u = -(right + left) / a
+/// v = -(top + bottom) / b
+/// w = -(far + near) / 2c
+/// @endcode
+/// @remarks
+/// A few properties of the transformation
+/// - the positive z-axis points out of the screen (negative z-axis points into the screen)
+/// - the positive x-axis points to the right
+/// - the positive y-axis points to the top
+static inline void
+idlib_matrix_4x4_f32_set_ortho
+	(
+		idlib_matrix_4x4_f32* target,
+		idlib_f32 left,
+		idlib_f32 right,
+		idlib_f32 bottom,
+		idlib_f32 top,
+		idlib_f32 near,
+		idlib_f32 far
+	);
+
 static inline void
 idlib_matrix_4x4_f32_set_identity
 	(
@@ -283,6 +326,50 @@ idlib_matrix_4x4_f32_set_rotation_z
 	target->e[3][3] = 1.f;
 }
 
+inline void
+idlib_matrix_4x4_f32_set_ortho
+	(
+		idlib_matrix_4x4_f32* target,
+		idlib_f32 left,
+		idlib_f32 right,
+		idlib_f32 bottom,
+		idlib_f32 top,
+		idlib_f32 near,
+		idlib_f32 far
+	)
+{
+	idlib_f32 a = right - left;
+	idlib_f32 b = top - bottom;
+	idlib_f32 c = far - near;
+
+	idlib_f32 u = -(right + left) / a;
+	idlib_f32 v = -(top + bottom) / b;
+	idlib_f32 w = -(far + near) / c;
+
+	// column #1
+	target->e[0][0] = 2.f / a;
+	target->e[1][0] = 0.f;
+	target->e[2][0] = 0.f;
+	target->e[3][0] = 0.f;
+
+	// column #2
+	target->e[0][1] = 0.f;
+	target->e[1][1] = 2.f / b;
+	target->e[2][1] = 0.f;
+	target->e[3][1] = 0.f;
+
+	// column #3
+	target->e[0][2] = 0.f;
+	target->e[1][2] = 0.f;
+	target->e[2][2] = -2.f / c;
+	target->e[3][2] = 0.f;
+
+	// column #4
+	target->e[0][3] = u;
+	target->e[1][3] = v;
+	target->e[2][3] = w;
+	target->e[3][3] = 1.f;
+}
 #if IDLIB_COMPILER_C == IDLIB_COMPILER_C_MSVC
 	#pragma pop_macro("near")
 	#pragma pop_macro("far")
